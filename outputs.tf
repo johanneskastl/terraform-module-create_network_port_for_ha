03dd36c0bc66e4be94063355efc64e53c9b53bc6
Johannes Kastl <kastl@b1-systems.de>
2019-11-30 21:24:57 +0100

    REFACTOR: output the complete port object instead of single attributes

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,5 @@
 #
 
-output "port_id" {
-  value = openstack_networking_port_v2.create_port.id
-}
-output "port_addresses" {
-  value = openstack_networking_port_v2.create_port.all_fixed_ips
+output "port" {
+  value = openstack_networking_port_v2.create_port
 }
